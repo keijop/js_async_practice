a8e9fa71edf10d79df84a1d845a71d6ab6990115
Keijo Prunt <keijo.prunt@vincit.fi>
2022-07-14 00:27:30 +0300

    Add loading spinner and functions using Promises

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 const form = document.querySelector('form');
+const main = document.querySelector('main');
+const loadingSpinner = document.querySelector('.loading_popup');
+
+const toggleLoadingSpinner = () => loadingSpinner.classList.toggle('loading');
 
 const geocodingBaseAPI =
   'http://www.mapquestapi.com/geocoding/v1/address?key=er3hjA7tECfpUhqpuk6aCeNPk4vGxdHq&location=';
@@ -11,10 +15,22 @@ const getCoordinates = async (input) => {
   return data.results[0].locations[0].latLng;
 };
 
+const getCoordinatesPromise = (input) => {
+  return fetch(`${geocodingBaseAPI}${input}`)
+    .then((resp) => resp.json())
+    .then((data) => data.results[0].locations[0].latLng);
+};
+
 const getWeather = async (lat, lng) => {
   const response = await fetch(`${weatherBaseAPI}&lat=${lat}&lon=${lng}`);
   const data = await response.json();
   return data;
+};
+
+const getWeatherPromise = (lat, lng) => {
+  return fetch(`${weatherBaseAPI}&lat=${lat}&lon=${lng}`)
+    .then((resp) => resp.json())
+    .then((data) => data);
 };
 
 const mapCloudIndexToText = {
@@ -29,33 +45,73 @@ const mapCloudIndexToText = {
   9: 'Very cloudy',
 };
 
-const renderResultsDOM = (temp, wind, cloudIndex, city) => {
-  const main = document.querySelector('main');
-  const weatherNode = document.createElement('section');
-  const generateParagraph = (element) => document.createElement(element);
+const createElement = (element) => document.createElement(element);
 
+const renderResultsDOM = (temp, wind, cloudIndex, city) => {
+  const weatherNode = createElement('section');
+
+  const header2Node = document.createTextNode(`${city.toUpperCase()} current weather:`);
   const tempNode = document.createTextNode(`Temperature: ${temp} C`);
   const cloudNode = document.createTextNode(`${mapCloudIndexToText[cloudIndex]}`);
   const windNode = document.createTextNode(`Wind speed: ${wind.speed} m/s, direction: ${wind.direction}`);
-  const h2Node = document.createTextNode(`${city.toUpperCase()} weather info:`);
 
-  weatherNode.appendChild(generateParagraph('h2')).appendChild(h2Node);
-  weatherNode.appendChild(generateParagraph('p')).appendChild(cloudNode);
-  weatherNode.appendChild(generateParagraph('p')).appendChild(tempNode);
-  weatherNode.appendChild(generateParagraph('p')).appendChild(windNode);
+  weatherNode.appendChild(createElement('h2')).appendChild(header2Node);
+  weatherNode.appendChild(createElement('p')).appendChild(cloudNode);
+  weatherNode.appendChild(createElement('p')).appendChild(tempNode);
+  weatherNode.appendChild(createElement('p')).appendChild(windNode);
 
   main.appendChild(weatherNode);
 };
 
-const handleSubmit = async (e) => {
-  const inputField = document.querySelector('input');
-  e.preventDefault();
-  const { lat, lng } = await getCoordinates(inputField.value);
-  const weatherData = await getWeather(lat, lng);
-  const { temp2m, cloudcover, wind10m } = weatherData.dataseries[0];
-  renderResultsDOM(temp2m, wind10m, cloudcover, inputField.value);
-  inputField.value = '';
-  inputField.focus();
+const renderErrorDOM = (error) => {
+  const errorNode = createElement('div');
+  const errMsgNode = createElement('b');
+  const errMsgTextNode = document.createTextNode(`Something went wrong: ${error}`);
+
+  errMsgNode.appendChild(errMsgTextNode);
+  errorNode.appendChild(errMsgNode);
+  main.appendChild(errorNode);
 };
 
-form.addEventListener('submit', handleSubmit);
+const handleSubmit = async (e) => {
+  try {
+    e.preventDefault();
+    toggleLoadingSpinner();
+    const inputField = document.querySelector('input');
+    const { lat, lng } = await getCoordinates(inputField.value);
+    const weatherData = await getWeather(lat, lng);
+    const { temp2m, cloudcover, wind10m } = weatherData.dataseries[0];
+    renderResultsDOM(temp2m, wind10m, cloudcover, inputField.value);
+    toggleLoadingSpinner();
+    inputField.focus();
+  } catch (error) {
+    renderErrorDOM(error);
+    toggleLoadingSpinner();
+  }
+};
+
+const handleSubmitWithPromises = (e) => {
+  e.preventDefault();
+  toggleLoadingSpinner();
+  const inputField = document.querySelector('input');
+  getCoordinatesPromise(inputField.value)
+    .then((coord) => getWeatherPromise(coord.lat, coord.lng))
+    .then((weatherData) => {
+      const { temp2m, cloudcover, wind10m } = weatherData.dataseries[0];
+      const city = inputField.value;
+      renderResultsDOM(temp2m, wind10m, cloudcover, city);
+      toggleLoadingSpinner();
+      inputField.focus();
+    })
+    .catch((err) => {
+      renderErrorDOM(err);
+      toggleLoadingSpinner();
+    });
+};
+
+const combinedHandler = (e) => {
+  handleSubmit(e);
+  handleSubmitWithPromises(e);
+};
+
+form.addEventListener('submit', combinedHandler);
